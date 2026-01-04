2070902b9e1406eef6292588d7cca929201e3d9a
Bikash Gosain <bikashgosain@gmail.com>
2026-01-03 18:46:24 -0800

    blogs app and blog model

---
+++ blogs/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Category, Blog
+
+# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}  # auto fill slug field from title field
+    list_display = ('title', 'author', 'category', 'status', 'is_featured', 'created_at')  # fields to be displayed in admin panel
+    search_fields = ( 'id', 'title', 'category__category_name', 'status')  # search by title and author's username
+    list_editable = ('is_featured', 'status')  # make is_featured and status editable in list display
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
---
+++ blogs/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.category_name
+    
+
+
+# status may be draft or published so we made dropdown
+STATUS_CHOICES = (
+    # (0, 'Draft'),
+    # (1, 'Published'),
+    ('Draft', 'Draft'),
+    ('Published', 'Published'),
+)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) # on delete category all posts related category will be deleted
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # on delete user all posts related user will be deleted
+    featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d/', blank=True)
+    short_description = models.TextField(max_length=200)
+    blog_body = models.TextField(max_length=5000)
+    status = models.CharField(max_length=20, default='Draft', choices=STATUS_CHOICES)  # draft = 0, published = 1 status may be draft or published so we made dropdown
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
